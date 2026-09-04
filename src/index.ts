cac1b65a01c3dc73199f51e7a1cbdd244f9ece13
luis gonzález <65631946+luisangeluis@users.noreply.github.com>
2026-09-04 22:25:35 +0000

    Test from codesanbox

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { App } from "./app";
 import { connectDb } from "./database/connectDb";
-import {mysqlDatabase, mysqlRootPassword, mysqlUser} from "./dev.env";
+import { mysqlDatabase, mysqlRootPassword, mysqlUser } from "./dev.env";
 
 const app = new App();
 
-async function main(){
-    await connectDb();
-    
-    app.listen(3000);
+async function main() {
+  //test from codesandbox
+  await connectDb();
+
+  app.listen(3000);
 }
 
 main();
