102ba038baac277c7afe6b68d2c43830d5a1761b
luis gonzález <65631946+luisangeluis@users.noreply.github.com>
2026-09-05 05:28:57 +0000

    Docker-compose:A waiting time was added to the service

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,27 @@
 import express, { Application } from "express";
 import { ProductContainer } from "./products/product.container";
 
-export class App{
-    private express: Application;
-    private readonly productContainer:ProductContainer
+export class App {
+  private express: Application;
+  private readonly productContainer: ProductContainer;
 
-    constructor() {
-        this.express = express();
-        this.express.use(express.json());
+  constructor() {
+    this.express = express();
+    this.express.use(express.json());
 
-        this.productContainer = new ProductContainer();
-        this.routes();
-    }
+    this.productContainer = new ProductContainer();
+    this.routes();
+  }
 
-    private routes(){
-        this.express.use("/products",this.productContainer.getRouter());
-    }
+  private routes() {
+    this.express.use("/products", this.productContainer.getRouter());
+    // this.express.use("/", (req, res) =>
+    //   res.json({ message: "api funcionando" })
+    // );
+  }
 
-    listen(port:number = 3000){
-        this.express.listen(port);
-        console.log("Server is running on port " + port);
-    }   
+  listen(port: number = 3000) {
+    this.express.listen(port);
+    console.log("Server is running on port " + port);
+  }
 }
